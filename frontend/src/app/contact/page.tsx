cf95d6644c73ab17fb4295f500151f2c85c0db06
saiKG <saikumar.gurugu@gmail.com>
2025-04-27 20:59:39 +1000

    trigger

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -76,8 +76,8 @@ const ContactPage = () => {
           Contact Me
         </h2>
         <p className="text-gray-700 dark:text-gray-300 mb-6">
-          {/* eslint-disable-next-line react/no-unescaped-entities */}
-          I'm always open to discussing new projects, ideas, or opportunities. Feel free to reach out using the form below, or connect with me on LinkedIn.
+         {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I am always open to discussing new projects, ideas, or opportunities. Feel free to reach out using the form below, or connect with me on LinkedIn.
         </p>
         <div className="mb-6 text-center">
           <a
